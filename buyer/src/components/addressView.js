dbce9e74de44078112270250ab6ec45b90668e63
An <anzhanghust@gmail.com>
2022-11-05 19:24:22 -0700

    add address check in

--- buyer/src/components/addressView.js
+++ buyer/src/components/addressView.js
@@ -23,26 +23,57 @@ export const AddressView = ({ setStep, tokenId, burnt }) => {
                     <TextField fullWidth id="outlined-basic" label="address" variant="outlined" value={address}
                         onChange={handleAddressChange} />
                     <Button onClick={async () => {
+
                         const walletAddress = await getWalletAddress();
-                        const body = {
-                            Address: address,
-                            TokenId: tokenId,
-                            ContractAddr: window.CONTRACT_ADDRESS,
-                            Redeemed: burnt
-                        }
-                        await fetch(API_DB + "ship/" + walletAddress, {
-                            method: "PUT",
-                            body: JSON.stringify(body),
-                            headers: {
-                                'Content-Type': 'application/json',
-                            }
+
+                        await fetch(API_DB + "ship/" + window.CONTRACT_ADDRESS, {
+                            method: "GET",
                         })
-                            .then(response => {
-                                return response
+                            .then((response) => response.json())
+                            .then(async (response) => {
+                                const dataFromDb = _.get(response, ["Item", "AddressData"]);
+                                console.log("ABI", dataFromDb);
+                                var body = "";
+                                if (dataFromDb) {
+                                    const addresArray = JSON.parse(dataFromDb);
+                                    if (addresArray.isArray()) {
+                                        addresArray.push({
+                                            WalletAddress: walletAddress,
+                                            Address: address,
+                                            TokenId: tokenId,
+                                            ContractAddr: window.CONTRACT_ADDRESS,
+                                            Redeemed: burnt
+                                        })
+                                        body = JSON.stringify(addresArray);
+                                    }
+                                }
+                                else {
+                                    body = JSON.stringify({
+                                        WalletAddress: walletAddress,
+                                        Address: address,
+                                        TokenId: tokenId,
+                                        ContractAddr: window.CONTRACT_ADDRESS,
+                                        Redeemed: burnt
+                                    });
+                                }
+                                await fetch(API_DB + "ship/" + window.CONTRACT_ADDRESS, {
+                                    method: "PUT",
+                                    body,
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                    }
+                                })
+                                    .then(response => {
+                                        return response
+                                    })
+                                    .catch(error => {
+                                        console.error(error);
+                                    });
                             })
-                            .catch(error => {
+                            .catch((error) => {
                                 console.error(error);
                             });
+
                         setStep(4);
                     }}>
                         Submit
